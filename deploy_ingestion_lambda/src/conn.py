0029bf3989ec6e87a4e94acd1a3492da9a7ff858
roshaka <roshaka@gmail.com>
2023-03-22 16:22:51 +0000

    Created and tested functions to query Totesys database

--- deploy_ingestion_lambda/src/conn.py
+++ deploy_ingestion_lambda/src/conn.py
@@ -1,23 +1,28 @@
+'''
+    conn.py contains utility functions which load the required credentials to access the databases from a secrets manager/.env
+'''
 import pg8000.native
 from dotenv import load_dotenv
 import os
 
-def get_db_password(db_name):
+def get_db_password(database_name):
+    '''Returns the password for database_name'''
     load_dotenv()
     lookup = {
         'totesys': 'totesys_password',
         'dw': 'dw_password'
     }   
-    return os.getenv(lookup[db_name])
+    return os.getenv(lookup[database_name])
 
 
-def get_db_name(db_name):
+def get_db_name(database_name):
+    '''Returns the database name for database_name'''
     load_dotenv()
     lookup = {
         'totesys': 'totesys_db_name',
         'dw': 'dw_db_name'
     }   
-    return os.getenv(lookup[db_name])
+    return os.getenv(lookup[database_name])
 
 def get_username():
     load_dotenv()
@@ -35,13 +40,12 @@ def get_region():
     load_dotenv()
     return os.getenv('region')
 
-def connect_to_database(db_name):
+def connect_to_database(database_name):
+    '''Establishes and returns a native pg8000 connection to database_name'''
     return  pg8000.native.Connection(
         user=get_username(), 
         host=get_host(), 
-        database=get_db_name(db_name), 
+        database=get_db_name(database_name), 
         port=get_port(), 
-        password=get_db_password(db_name)
-        )
-
-    
+        password=get_db_password(database_name)
+    )
---
+++ deploy_ingestion_lambda/src/main.py
@@ -0,0 +1,16 @@
+from deploy_ingestion_lambda.src.conn import connect_to_database
+from deploy_ingestion_lambda.src.queries import *
+
+conn_totesys = connect_to_database('totesys')
+
+# get_staff_data(conn_totesys)
+
+# get_all_table_names(conn_totesys)
+# get_table_column_names(conn_totesys, 'staff')
+
+dicts = create_list_of_dictionaries(conn_totesys, 'staff')
+print(dicts[0])
+
+# print(get_table_column_names(conn_totesys, 'staff'))
+
+conn_totesys.close()
