3c41013724b02bab9163b92b3f6768c55110423d
Helen Morris <morris_helen@hotmail.com>
2023-03-24 10:51:48 +0000

    updated data archive_file to source_dir and add excludes tests

--- terraform/data.tf
+++ terraform/data.tf
@@ -4,6 +4,7 @@ data "aws_region" "current_region" {}
 
 data "archive_file" "ingestion-lambda" {
     type = "zip"
-    output_path = "${path.module}/../ingestion-function.zip"
-    source_file = "${path.module}/../deploy_ingestion_lambda/src/main.py"
+    output_path = "${path.module}/../deploy_ingestion_lambda.zip"
+    source_dir = "${path.module}/../deploy_ingestion_lambda"
+    excludes = "${path.module}/../deployment_ingestion_lambda/test.zip"
 }
