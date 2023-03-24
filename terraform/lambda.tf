7b6f86c205718db82873cf688390bf792b806b7e
Ezenwa Ugbomah <eze.ugbomah@gmail.com>
2023-03-24 17:54:04 +0000

    uploaded deployment zip file into s3 bucket and edited ingestion lamda function

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,5 +1,6 @@
 resource "aws_lambda_function" "ingest_lambda" {
-    filename = "${path.module}/../deployment_requirements.zip"
+    # filename = "${path.module}/../deployment_requirements.zip"
+    filename = data.aws_s3_bucket_object.lambda_code_deployment.id
     function_name = var.ingestion_lambda_name
     role = aws_iam_role.lambda_ingest_role.arn
     handler = "deployment_requirements.lambda_handler.ingest"
