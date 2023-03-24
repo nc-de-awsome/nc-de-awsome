9e36f17c10cf673b62b8c1e6cdd4cb100d72b3ea
Helen Morris <morris_helen@hotmail.com>
2023-03-24 09:25:46 +0000

    update source_dir to source_file

--- terraform/data.tf
+++ terraform/data.tf
@@ -5,5 +5,5 @@ data "aws_region" "current_region" {}
 data "archive_file" "ingestion-lambda" {
     type = "zip"
     output_path = "${path.module}/../ingestion-function.zip"
-    source_dir = "${path.module}/../src"
+    source_file = "${path.module}/../deploy_ingestion_lambda/src/main.py"
 }
