e0dafa5cca331a497871796c3bc4f7e6c14bf8de
roshaka <roshaka@gmail.com>
2023-03-25 14:59:14 +0000

    Wrote code for dim table generation functions

---
+++ deployment_requirements/lambda_handler.py
@@ -0,0 +1,167 @@
+# from deploy_ingestion_lambda.src.conn import connect_to_database
+# from deploy_ingestion_lambda.src.queries import *
+# from deploy_ingestion_lambda.src.errors import IngestionError
+print('are we here?')
+import platform
+print('version: ',platform.python_version())
+import json
+print('how about here?')
+
+import boto3
+print('and now?')
+import pg8000.native
+def ingest(event, context):
+    try:
+        conn = connect_to_database()
+        table_names = get_all_table_names(conn)
+        for table in table_names:
+            rows = create_list_of_dictionaries(conn, table)
+            json = list_of_dictionaries_to_json(rows)
+            write_json_to_bucket(json, 'nc-de-awsome-ingestion-zone', f'totesys/{table}.json' )
+        conn.close()
+    except Exception as e:
+        raise IngestionError(f'{e}')
+
+class AwsomeError(Exception):
+    pass
+
+class DatabaseConnectionError(AwsomeError):
+    pass
+
+class IngestionError(AwsomeError):
+    pass
+
+class WriteError(AwsomeError):
+    pass
+
+class SelectQueryError(AwsomeError):
+    pass
+
+def get_secret(key):
+    sm = boto3.client('secretsmanager')
+    return sm.get_secret_value(SecretId = key)
+
+def get_db_password():
+    return get_secret('TOTESYS_PASSWORD')
+
+def get_db_name():
+    return get_secret('TOTESYS_DATABASE_NAME')
+
+def get_username():
+    return get_secret('TOTESYS_USERNAME')
+
+def get_host():
+    return get_secret('TOTESYS_HOST')
+
+def get_port():
+    return get_secret('TOTESYS_PORT')
+
+def get_region():
+    return get_secret('TOTESYS_REGION')
+
+def connect_to_database():
+    '''Establishes and returns a native pg8000 connection to database_name'''
+    try:
+        return  pg8000.native.Connection(
+            user=get_username(), 
+            host=get_host(), 
+            database=get_db_name(), 
+            port=get_port(), 
+            password=get_db_password()
+        )
+    except:
+        raise DatabaseConnectionError('Unable to connect to Totesys database')
+
+def get_all_table_names(conn):
+    '''Returns a list of table_name strings of each table in Totesys database
+    
+        parameters:
+            conn: pg8000.native.Connection
+        
+        returns:
+            list of strings 
+    '''
+    try:
+        tables = conn.run("SELECT table_name FROM information_schema.tables WHERE table_type='BASE TABLE' AND table_schema='public';")
+        table_names = [table[0] for table in tables]
+        return table_names
+    except:
+        raise SelectQueryError('Unable to select table_names from totesys')
+
+def _get_table_column_names(conn, table_name):
+    '''Returns a list of column_name strings in table_name
+
+        parameters:
+            conn: pg8000.native.Connection
+            table_name: string
+        
+        returns:
+            list of strings 
+    '''
+    try:
+        columns = conn.run(f"SELECT column_name FROM information_schema.columns WHERE table_name = '{table_name}' AND table_schema='public';")
+        column_names = [column_name[0] for column_name in columns]
+        return column_names
+    except:
+        raise SelectQueryError(f'Unable to get columns from {table_name}')
+
+def _get_table_values(conn, table_name):
+    '''Returns a list of lists of values in table_name
+    
+        parameters:
+            conn: pg8000.native.Connection
+            table_name: string
+        
+        returns:
+            list of strings 
+    '''
+    try:
+        values = conn.run(f'SELECT * FROM {table_name};')
+        return values
+    except:
+        raise SelectQueryError(f'Unable to select values from {table_name}')
+
+def create_list_of_dictionaries(conn, table_name):
+    '''Returns a list of dicts of column/value pairs from table_name
+    
+        parameters:
+            conn: pg8000.native.Connection
+            table_name: string
+        
+        returns:
+            list of dicts 
+
+    '''
+    columns = _get_table_column_names(conn, table_name)
+    values = _get_table_values(conn, table_name)
+    
+    column_value_pairs = []
+
+    for v in values:
+        dict = {}
+        for i in range(len(columns)):
+            dict[columns[i]] = v[i]
+        
+        column_value_pairs.append(dict)
+
+    return column_value_pairs
+
+def list_of_dictionaries_to_json(list_of_dicts):
+    '''Returns a (JSON) string from a list of dicts
+        
+        parameters:
+            list_of_dicts: list of dicts
+
+        returns:
+            JSON (string)
+    '''
+    return json.dumps(list_of_dicts, indent=4, default=str)
+
+def write_json_to_bucket(json, bucket_name, key):
+    response = None
+    try:
+        s3 = boto3.client('s3')
+        response = s3.put_object(Body=json, Bucket=bucket_name, Key=key)
+    except:
+        raise WriteError('Unable to write JSON to S3 bucket')
+    return response
--- main.py
+++ main.py
@@ -1,33 +1,51 @@
 import pandas as pd
+import requests
+import json
+import os
 from datetime import datetime
 
-def generate_dim_staff_table():
+def transform(event, context):
+    # load all data from transformation bucket
     staff_df = load_data_frame_from_json('staff')
-    select_from_staff_df = staff_df[
-        [
-            'staff_id',
-            'first_name',
-            'last_name',
-            'email_address',
-            'department_id'
-        ]
-    ]
-
     department_df = load_data_frame_from_json('department')
-    select_from_department_df = department_df[
-        [
-            'department_id',
-            'department_name',
-            'location'
-        ]
-    ]
+    design_df = load_data_frame_from_json('design')
+    address_df = load_data_frame_from_json('address')
+    counterparty_df = load_data_frame_from_json('counterparty')
+    sales_order_df = load_data_frame_from_json('sales_order')
+    currency_df = load_data_frame_from_json('currency')
+    payment_df = load_data_frame_from_json('payment')
+    transaction_df = load_data_frame_from_json('transaction')
 
-    join = select_from_staff_df.join(
-        select_from_department_df.set_index('department_id'),
-        on = 'department_id'
-    )
+    # get/load other data
+    currency_name_df = load_data_frame_from_csv('./other_data/currencies.csv')
+    update_forex_rates()
 
-    dim_staff = join[
+    # transform data into fact and dim tables
+    dim_staff = generate_dim_staff_table(staff_df, department_df)
+    dim_design = generate_dim_design(design_df)
+    dim_location = generate_dim_location(address_df)
+    dim_counterparty = generate_dim_counterparty(counterparty_df, address_df)
+    dim_date = generate_dim_date(sales_order_df)
+    dim_currency = generate_dim_currency(currency_df, currency_name_df)
+    dim_payment = generate_dim_payment_type(payment_df, transaction_df)
+    dim_transaction = generate_dim_transaction(transaction_df)
+
+    # writeout fact/dim tables to parquet to load bucket
+    write_data_frame_to_parquet(dim_staff, 'dim_staff')
+    write_data_frame_to_parquet(dim_design, 'dim_design')
+    write_data_frame_to_parquet(dim_location, 'dim_location')
+    write_data_frame_to_parquet(dim_counterparty, 'dim_counterparty')
+    write_data_frame_to_parquet(dim_date, 'dim_date')
+    write_data_frame_to_parquet(dim_currency, 'dim_currency')
+    write_data_frame_to_parquet(dim_payment, 'dim_payment')
+    write_data_frame_to_parquet(dim_transaction, 'dim_transaction')
+
+def generate_dim_staff_table(staff_df, department_df):
+    return staff_df.join(
+        department_df.set_index('department_id'),
+        on = 'department_id',
+        lsuffix='_L'
+    )[
         [
             'staff_id',
             'first_name',
@@ -37,11 +55,9 @@ def generate_dim_staff_table():
             'email_address'
         ]
     ]
-    return dim_staff
 
-def generate_dim_design():
-    design_df = load_data_frame_from_json('design')
-    dim_design = design_df[
+def generate_dim_design(design_df):
+    return design_df[
         [
             'design_id',
             'design_name',
@@ -49,11 +65,9 @@ def generate_dim_design():
             'file_name'
         ]
     ]
-    return dim_design
 
-def generate_dim_location():
-    address_df = load_data_frame_from_json('address')
-    address_columns = address_df[
+def generate_dim_location(address_df):
+    return address_df[
         [
             'address_id', 
             'address_line_1', 
@@ -64,24 +78,17 @@ def generate_dim_location():
             'country',
             'phone'
         ]
-    ]
-    
-    dim_location = address_columns.rename(
+    ].rename(
         columns={
             'address_id' : 'location_id'
         }
     )
-    return dim_location
 
-def generate_dim_counterparty():
-    counterparty_df = load_data_frame_from_json('counterparty')
-    address_df = load_data_frame_from_json('address')
-
-    return pd.merge(
-        counterparty_df,
-        address_df,
-        left_on = 'legal_address_id',
-        right_on = 'address_id',
+def generate_dim_counterparty(counterparty_df, address_df):
+    return counterparty_df.join(
+        address_df.set_index('address_id'),
+        on = 'legal_address_id',
+        lsuffix='_L',
     )[
         [
             'counterparty_id',
@@ -106,18 +113,18 @@ def generate_dim_counterparty():
         }
     )
 
-def generate_dim_date():
-    # sales_order.created_at
-    sales_order_df = load_data_frame_from_json('sales_order')
-    print(sales_order_df)
-    sales_timestamps=sales_order_df[['created_at']]
-    # sales_timestamps.info()
-    # print(sales_timestamps)
+def generate_dim_date(sales_order_df):
+    sales_timestamps=sales_order_df[
+        [
+            'created_at'
+        ]
+    ]
 
-    # def create_datetime(timestamp)
+    def create_datetime(timestamp):
+        return pd.to_datetime(pd.Timestamp(timestamp).to_pydatetime().replace(microsecond=0))
 
-    datetimes = [pd.to_datetime(pd.Timestamp(s[0]).to_pydatetime().replace(microsecond=0)) for s in sales_timestamps.values]
     dicts = []
+    datetimes = [create_datetime(s[0]) for s in sales_timestamps.to_numpy()]
     for date in datetimes:
         dicts.append(
                 {
@@ -132,33 +139,112 @@ def generate_dim_date():
             }
         )
     
-    # x = datetimes[0].strftime('%y-%m-%d %H:%M:%S')
-    
     return pd.DataFrame.from_records(dicts)
- 
+
+def generate_dim_currency(currency_df, currency_name_df):
+    return currency_df.join(
+        currency_name_df.set_index('CurrencyCode'),
+        on='currency_code'
+    )[
+        [
+            'currency_id',
+            'currency_code',
+            'CurrencyName'
+        ]
+    ].rename(
+        columns={
+            'CurrencyName' : 'currency_name'
+        }
+    )
+
+def generate_dim_payment_type(payment_df, transaction_df):
+    return payment_df.join(
+        transaction_df,
+        on='transaction_id',
+        lsuffix='_L'
+    )[
+        [
+            'payment_type_id',
+            'transaction_type',
+        ]
+    ].rename(
+        columns={
+            'transaction_type' : 'payment_type_name'
+        }
+    )
+
+def generate_dim_transaction(transaction_df):
+    return transaction_df.where(pd.notnull(transaction_df), None)[
+        [
+            'transaction_id',
+            'transaction_type',
+            'sales_order_id',
+            'purchase_order_id'
+        ]
+    ]
+
+def update_forex_rates():
+    '''Gets forex rates data, writes to file, and updates at approx 8am each day'''
+    forex_log_path = './other_data/forex_rates_log.json'
+    
+    now = datetime.now()
+
+    try:
+        if os.path.exists(forex_log_path):
+            with open(forex_log_path, 'r') as f:
+                forex = json.loads(f.read())
+                last_check = datetime.utcfromtimestamp(forex['last_updated'])
+
+                if abs(now.day-last_check.day) == 0 or now.hour < 8 :
+                    print('No need to update forex rates')
+                    return
+    except:
+        print('Could not find/read forex rate log; will get forex rates')
+    
+    currency_pairs=[
+        'GBPUSD',
+        'EURGBP',
+        'EURUSD',
+        'USDEUR',
+        'USDGBP',
+    ]
+    
+    rates = []
+    for cp in currency_pairs:
+        response =requests.get(f'https://www.freeforexapi.com/api/live?pairs={cp}')
+        rate = json.loads(response.text)['rates']
+        dict = {cp : rate[cp]['rate'] }
+        rates.append(dict)
+
+    with open('./other_data/forex_rates.json', 'w') as f:
+        f.write(json.dumps(rates, indent=4))
+    
+    with open(f'{forex_log_path}', 'w') as f:
+        info =   {'last_updated':now.timestamp()}
+        f.write(json.dumps(info))
+    
+    print('Updated forex rates')
+
 # utilities
 
 def load_data_frame_from_json(table_name):
     return pd.read_json(f'./totesys_json/{table_name}.json')
 
-def write_data_frame_to_parquet(data_frame, file_name):
-    data_frame.to_parquet(f'./transformation_parquet/{file_name}.parquet', engine='auto', compression=None, index=False)
+def load_data_frame_from_csv(filepath):
+    return pd.read_csv(f'{filepath}')
 
-    ######### temp code #########
+def write_data_frame_to_parquet(data_frame, file_name):
+    data_frame.to_parquet(
+        f'./transformation_parquet/{file_name}.parquet', 
+        engine='auto', 
+        compression=None, 
+        index=False
+    )
+##################### temp code #####################
+    write_data_frame_to_local_txt(data_frame, file_name)
+
+##################### temp code #####################
+def write_data_frame_to_local_txt(data_frame, file_name):
     with open(f'./transformation_parquet/{file_name}.txt', 'w') as f:
         f.write(data_frame.to_string())
-    ########### end ############
 
-# run
-
-# dim_staff = generate_dim_staff_table()
-# dim_design = generate_dim_design()
-# dim_location = generate_dim_location()
-# dim_counterparty = generate_dim_counterparty()
-dim_date = generate_dim_date()
-
-# write_data_frame_to_parquet(dim_staff, 'dim_staff')
-# write_data_frame_to_parquet(dim_design, 'dim_design')
-# write_data_frame_to_parquet(dim_location, 'dim_location')
-# write_data_frame_to_parquet(dim_counterparty, 'dim_counterparty')
-write_data_frame_to_parquet(dim_date, 'dim_date')
