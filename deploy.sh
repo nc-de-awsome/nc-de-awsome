d3175673dbf7f379f915be3562f246ab755f862e
roshaka <roshaka@gmail.com>
2023-03-27 12:07:52 +0100

    Fixed deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,51 +1,17 @@
 #!/bin/bash
-while loop==true
-echo "Ready to deploy? [y/n]"
-read input
-do
-    if [ $input == 'n' ] || [ $input == 'y' ]; then
-        loop=false
-        break
-    fi
-    echo "Invalid input"
-done
 
-if [ $input == 'n' ]; then
-    echo "Aborted"
-else
-
-LAMBDA_FILE="lambda_handler.py"
-LAMBDA_HANDLER_PATH="./deploy_ingestion_lambda/$LAMBDA_FILE"
-ZIP_FILE="deployment_requirements.zip"
-ZIP_PATH="./dist/$ZIP_FILE"
-INGEST_PACKAGE="dist/ingest_package"
-
-mkdir -p $INGEST_PACKAGE
-cp $LAMBDA_HANDLER_PATH ./dist/$LAMBDA_FILE
-
-if [ -f $ZIP_PATH ]; then
-    rm $ZIP_PATH
+if [ -f ./dist/deploy_ingestion_lambda.zip ]; then
+    rm ./dist/deploy_ingestion_lambda.zip
 fi
 
-pip install pg8000 --target $INGEST_PACKAGE
-pip install boto3 --target $INGEST_PACKAGE
-zip -r dist/$ZIP_FILE $INGEST_PACKAGE
-zip dist/$ZIP_FILE dist/$LAMBDA_FILE
-rm -r $INGEST_PACKAGE
-rm dist/$LAMBDA_FILE
+mkdir -p dist/ingest_package
 
-GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+cp ./deploy_ingestion_lambda/lambda_handler.py ./dist/ingest_package/lambda_handler.py
+pip install pg8000 --target dist/ingest_package/
+pip install boto3 --target dist/ingest_package/
 
-# echo "Enter commit message in or [x] to abort:"
-# read msg2;
-
-# if [ $msg2 != 'x' ]; then
-#     git add . && git commit -m "$msg2" && git push origin $GIT_BRANCH
-#     git checkout main
-#     git merge $GIT_BRANCH
-#     git checkout $GIT_BRANCH
-# else
-#     echo "Aborted"
-# fi
-
-fi
+cd dist/ingest_package/
+zip -r ../deploy_ingestion_lambda.zip .
+cd ..
+cd ..
+rm -r dist/ingest_package
