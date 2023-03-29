aab80bc37d01076f1ae6391381d2a763ac73c90f
Helen Morris <morris_helen@hotmail.com>
2023-03-29 15:56:43 +0100

    create s3 object for process lambda code

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -10,8 +10,14 @@ resource "aws_s3_bucket" "lambda_bucket" {
   bucket = "nc-de-awsome-code-bucket"
 }
 
-resource "aws_s3_object" "lambda_code_deployment" {
+resource "aws_s3_object" "ingest_lambda_code_deployment" {
   bucket = aws_s3_bucket.lambda_bucket.bucket
   key    = "deploy_ingestion_lambda.zip"
   source = "${path.module}/../deployment_zips/deploy_ingestion_lambda.zip"
+}
+
+resource "aws_s3_object" "process_lambda_code_deployment" {
+  bucket = aws_s3_bucket.lambda_bucket.bucket
+  key    = "deploy_process_lambda.zip"
+  source = "${path.module}/../deployment_zips/deploy_process_lambda.zip"
 }
