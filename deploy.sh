6a641c9ca46f01ae866f43537bbc25ee64cd0b90
CamMacCodes <cameronjmaciver@gmail.com>
2023-03-30 09:45:19 +0100

    adds process lambda deployment package zip to deploy.sh script, specifying requirements lambda will need to reduce zip file size below 100MB

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# creates ingestion lambda deployment package zip
 if [ -f ./deployment_zips/deploy_ingestion_lambda.zip ]; then
     rm ./deployment_zips/deploy_ingestion_lambda.zip
 fi
@@ -16,3 +17,22 @@ zip -r ../deploy_ingestion_lambda.zip .
 cd ..
 cd ..
 rm -r deployment_zips/ingest_package
+
+# creates process lambda deployment package zip
+if [ -f ./deployment_zips/deploy_process_lambda.zip ]; then
+    rm ./deployment_zips/deploy_process_lambda.zip
+fi
+
+mkdir -p deployment_zips/process_package
+
+cp ./deploy_processed_lambda/main.py ./deployment_zips/process_package/main.py
+pip install pandas --target deployment_zips/process_package/
+pip install pyarrow --target deployment_zips/process_package/
+pip install fastparquet --target deployment_zips/process_package/
+pip install boto3 --target deployment_zips/process_package/
+pip install pytz --target deployment_zips/process_package/
+cd deployment_zips/process_package/
+zip -r ../deploy_process_lambda.zip .
+cd ..
+cd ..
+rm -r deployment_zips/process_package
